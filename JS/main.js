f441f55e9570f97c3cf4c41a4a042c999c5149cc
DavideAtzo <atzo9477@gmail.com>
2023-02-20 15:04:52 +0100

    inserimento di input prompt

--- JS/main.js
+++ JS/main.js
@@ -1,1 +1,6 @@
-'use strict'
+'use strict';
+
+// Grazie a questi, l'utente potrà inserire gli input grazie al prompt
+const firstName = prompt('Inserirsci il tuo nome');
+const lastName = prompt('Inseririsci il tuo cognome');
+const favColor = prompt('Inserisci il tuo colore preferito');
