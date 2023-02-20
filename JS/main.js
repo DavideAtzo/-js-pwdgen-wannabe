ece2629a20da28e6e7fd52953d91d9cbf9ca285b
DavideAtzo <atzo9477@gmail.com>
2023-02-20 15:38:07 +0100

    visualizzazione a schermo degli input

--- JS/main.js
+++ JS/main.js
@@ -7,3 +7,9 @@ const favColor = prompt('Inserisci il tuo colore preferito');
 
 // Creazione password combinando gli input inseriti dall'utente 
 const yourPassword = `${firstName}${lastName}${favColor}89`;
+
+// Inerisco i dati negli id per la visualizzazione degli input
+document.getElementById('firstName').innerHTML = firstName;
+document.getElementById('lastName').innerHTML = lastName;
+document.getElementById('favColor').innerHTML = favColor;
+document.getElementById('yourPassword').innerHTML = yourPassword;
